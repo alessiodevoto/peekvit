fix masks for flop computation

models/blocks.py
@@ -91,7 +91,7 @@ class SelfAttention(nn.Module):
         self.self_attention = nn.MultiheadAttention(input_dim, num_heads, batch_first=True, dropout=dropout)
 
     def forward(self, x):
-        out, weights = self.self_attention(query=x, key=x, value=x, need_weights=True)
+        out, weights = self.self_attention(x, x, x, need_weights=True)
         return out
 
 

models/residualvit.py
@@ -209,6 +209,7 @@ class ResidualViTBlock(ResidualModule):
 
         # residual gating, here we learn a scalar for each token
         self.mask = self.residual_gate(img_tokens, budget=current_budget, threshold=threshold)
+        # add ones for special tokens
         masked_tokens = self.mask * img_tokens 
         
 
@@ -230,12 +231,14 @@ class ResidualViTBlock(ResidualModule):
 
 
     def plain_forward(self, input: torch.Tensor):
-        x = self.ln_1(input)
-        x = self.self_attention(x)
+        expanded_mask = torch.cat([torch.ones((self.mask.size(0), 1, self.mask.size(2)), device=self.mask.device), self.mask, torch.ones((self.mask.size(0), 1, self.mask.size(2)), device=self.mask.device)], dim=1)
+        x = expanded_mask * self.ln_1(input) 
+        
+        x = expanded_mask * self.self_attention(x)
         x = self.dropout(x)
         x = x + input
 
-        y = self.ln_2(x)
+        y = expanded_mask * self.ln_2(x)
         y = self.mlp(y)
         return x + y
 
@@ -560,8 +563,7 @@ class ResidualVisionTransformer(nn.Module):
         # Add budget token
         if self.budget:
             x = self._add_budget_token(x)
-
-
+        
         # Pass through the encoder
         x = self.encoder(x)
 